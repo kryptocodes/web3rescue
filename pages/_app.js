2aee38c92276157d688ad95d1e556d30d0cf6111
kryptocodes <srivatsantb@gmail.com>
2022-12-04 02:40:58 +0530

    updated api url
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import "@biconomy/web3-auth/dist/src/style.css"
 
-export const API = 'https://9843-2406-7400-56-9a4e-20da-f9f0-d069-63c7.in.ngrok.io/ethindia'
+export const API = 'https://api.web3rescue.com/ethindia'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
